8b63b5b05bcf610a0f35c9604b8149a2a9cac339
Toks <tokslaw@chiefsoft.com>
2026-03-18 18:12:15 -0400

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,17 @@
+'''
+This models.py file defines the data structures for a food ordering app. It includes the following classes:
+***User
+Represents the customer using the app. Stores the user’s basic details and keeps track of their past orders.
+
+***MenuCatalog
+Represents the full list of available food and drink items. It helps organize and filter items by category.
+
+***MenuItem
+Represents a single food or drink product. Stores details such as name, price, category, and popularity rating.
+
+***Order
+Represents the items a user selects in one purchase request. It groups chosen menu items and calculates the total cost.
+
+***Transaction
+Represents the payment record linked to an order. It stores payment details such as amount paid, payment status, method, and date.
+'''
